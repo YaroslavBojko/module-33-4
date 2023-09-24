0daebe9703b53840f516e9552678756a391184a4
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-09-24 11:55:52 +0300

    input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+template <typename T>
+void input(T array[8])
+{
+    std::cout << "Fill the array (8): ";
+    for (int i = 0; i < 8; ++i)
+    {
+        std::cin >> array[i];
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int array[8];
+    //double array[8];
+
+    input(array);
+
+    for (int i = 0; i < 8; ++i)
+    {
+        std::cout << array[i] << " ";
+    }
 }
